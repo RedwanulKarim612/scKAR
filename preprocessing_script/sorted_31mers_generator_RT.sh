4f68d277a9f1e2d85be43df4a1a61bea81a3bd4a
Md. Redwanul Karim <58245255+RedwanulKarim612@users.noreply.github.com>
2026-03-07 21:03:52 +0600

    add output path as parameter

--- preprocessing_script/sorted_31mers_generator_RT.sh
+++ preprocessing_script/sorted_31mers_generator_RT.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Argument takes the genome.fa file.
+# Argument takes the genome.fa.gz file and the output file path
 
-gunzip -c $1 | jellyfish count -m 31 -s 100M -t 8 -o $jf_path /dev/fd/0
-jellyfish dump $jf_path > $fa_path
+gunzip -c $1 | jellyfish count -m 31 -s 100M -t 8 -o $1.jf /dev/fd/0
+jellyfish dump $1.jf > $1.fa
 
-awk '!/^>/' $1 | sort > 31_mers_only.txt
+awk '!/^>/' $1.fa | sort > $2
