df74790297b7cfdba7118653d4f428302da9e931
Md. Redwanul Karim <58245255+RedwanulKarim612@users.noreply.github.com>
2026-03-08 16:31:03 +0600

    fix file name

--- src/merge_kmers/combine_p_values.sh
+++ src/merge_kmers/combine_p_values.sh
@@ -9,11 +9,11 @@ if [ "$merge_method" = "hmp" ]; then
   Rscript hmp.R "$path/abyss/B_contigs.tsv" "$path/abyss/B_kmers.tsv" "$path/abyss/B_contigs_pvalues.tsv" 
   
 elif [ "$merge_method" = "fisher" ]; then
-  python fisher.py "$path/abyss/A_contigs.tsv" "$path/abyss/A_kmers.tsv" "$path/abyss/A_contigs_pvalues.tsv" "$merge_method"
-  python fisher.py "$path/abyss/B_contigs.tsv" "$path/abyss/B_kmers.tsv" "$path/abyss/B_contigs_pvalues.tsv" "$merge_method"
+  python fisherAndStouffer.py "$path/abyss/A_contigs.tsv" "$path/abyss/A_kmers.tsv" "$path/abyss/A_contigs_pvalues.tsv" "$merge_method"
+  python fisherAndStouffer.py "$path/abyss/B_contigs.tsv" "$path/abyss/B_kmers.tsv" "$path/abyss/B_contigs_pvalues.tsv" "$merge_method"
 elif [ "$merge_method" = "stouffer" ]; then
-  python fisher.py "$path/abyss/A_contigs.tsv" "$path/abyss/A_kmers.tsv" "$path/abyss/A_contigs_pvalues.tsv" "$merge_method"
-  python fisher.py "$path/abyss/B_contigs.tsv" "$path/abyss/B_kmers.tsv" "$path/abyss/B_contigs_pvalues.tsv" "$merge_method"
+  python fisherAndStouffer.py "$path/abyss/A_contigs.tsv" "$path/abyss/A_kmers.tsv" "$path/abyss/A_contigs_pvalues.tsv" "$merge_method"
+  python fisherAndStouffer.py "$path/abyss/B_contigs.tsv" "$path/abyss/B_kmers.tsv" "$path/abyss/B_contigs_pvalues.tsv" "$merge_method"
 else
   echo "Unsupported merge method: $merge_method. Please choose 'hmp', 'fisher', or 'stouffer'."
   exit 1
